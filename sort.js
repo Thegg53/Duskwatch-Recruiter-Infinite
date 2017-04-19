9dccbc2260de479a2c4b9f892a0a76fcacba9637
Guilherme Morgado <grmorgado@gmail.com>
2017-04-19 23:32:36 +0100

    Update (details positions of other cards)
    Minor code cleanup, now details the position of the rest of the deck.
    Card that starts on top is the card with value 0.001, 2nd card is 0.002 , 3rd is 0.003 etc

--- sort.js
+++ sort.js
@@ -7,7 +7,6 @@ function sort(sD,P1, P2, P3){
 //    alert("Size: " + sD + " \n starting position of top C: " + P1
 //    + " \n starting position of C wanted 2nd: " + P2
 //    + " \n starting position of C wanted 3rd: "+ P3);
- 
       var aIterations = [];
       var iterationCounter = 0;
  
@@ -16,8 +15,9 @@ function sort(sD,P1, P2, P3){
             alert("Divisible by 3. This only works if not divisible by 3");
       } else {
             var aD = [];
-            for (var j = 0; j < sD; j++){
-                  aD[j] = 0;
+			
+            for ( j = 0; j < sD; j++){
+                  aD[j] = (j + 1)/1000;
             }
             aD[P1 - 1] = 1;
             aD[P2 - 1] = 2;
@@ -25,48 +25,37 @@ function sort(sD,P1, P2, P3){
                  
             var jj;
 			// model to bring 3 cards to the top, accepting random order of top 3
-            while( aD[0] == 0 || aD[1] == 0 || aD[2] == 0){ // while the top 3 cards are not the 3 chosen
-                  j = [aD[0], aD[1], aD[2]]; //temporary store of top 3
+            while( aD[0] < 1 || aD[1] < 1 || aD[2] < 1){ // while the top 3 cards are not the 3 chosen
+                  j = [(aD[0]), (aD[1]), (aD[2])]; //temporary store of top 3
                   aD.splice(0,3); // remove the top 3
-                  jj = j.sort(); //sort top 3 (Unneeded -> Needed on top (1st) -> Needed 2nd -> Needed 3rd)
-                  if(haveWantedC == true){ //if we have found a card we want in the previous iteration, we now put the new cards found in the top of the bottom 3. This way, the newly found cards will group with the cards that were in the bottom of the previous pile.
-                        aD.push(jj[2]);
-                        aD.push(jj[1]);
-                        aD.push(jj[0]);
-                        var tempDeck = aD.slice();
-                        aIterations.push(tempDeck);
-                        iterationCounter++;
-                  } else {
-                        if(jj[0] == 1 || jj[1] == 1 || jj[2] == 1 ){ //if we found a single card, send it to the bottom of the pile.
+                  
+				  jj = j.sort(); //sort top 3 (Unneeded -> Needed 1st -> needed 2nd -> needed 3rd)
+
+                 console.log(jj);
+				  
+             
+					if(jj[0] == 1|| jj[1] == 1 || jj[2] == 1 ){ //if we found card number 1, send it to the bottom of the pile.
                               aD.push(jj[0]);
                               aD.push(jj[1]);
                               aD.push(jj[2]);
                               var tempDeck2 = aD.slice();
                               aIterations.push(tempDeck2);
+							  console.log("\n\n iteration number " + iterationCounter + " deck order:" + aIterations[iterationCounter])
                               iterationCounter++;
-                        } else { //in any other case (found 2 cards, 3 cards or none at all) put the cards we want on top of the pile and the remainder on bottom of pile. 
+                        } else { //in any other case put the cards we want on top of the pile and the remainder on bottom of pile. 
                               aD.push(jj[2]);
                               aD.push(jj[1]);
                               aD.push(jj[0]);
                               var tempDeck3 = aD.slice();
                               aIterations.push(tempDeck3);
+							  console.log("\n\n iteration number " + iterationCounter + " deck order:" + aIterations[iterationCounter])
                               iterationCounter++;
                         }
-                  }
- 
-                  if(aD[sD-3] != 0 || aD[sD-2] != 0 || aD[sD-1] != 0 ){
-                        haveWantedC = true;
-                  } else {haveWantedC = false;}
- 
-                  console.log(iterationCounter);
-                  if(iterationCounter > 50000){
+                
+                  if(iterationCounter > 5000){
                         break
                   }
             }
             alert("finished in " + iterationCounter + "iterations \n");
       }
- for(var i = 0; i < iterationCounter; i++){
-	console.log("\n\n iteration number " + i + " deck order:" + aIterations[i])	
-}
- 
 }
